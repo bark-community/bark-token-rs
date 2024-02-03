8e6b3f2dc4b5ddaf3e52f63c61a009e9c3e8b665
Stream Protocol <105049869+stream-protocol@users.noreply.github.com>
2024-02-03 12:21:20 +0200

    Create index.test.ts

---
+++ test/src/index.test.ts
@@ -0,0 +1,24 @@
+describe("Test", () => {
+  it("Airdrop", async () => {
+    // Fetch my balance
+    const balance = await pg.connection.getBalance(pg.wallet.publicKey);
+    console.log(`My balance is ${balance} lamports`);
+
+    // Airdrop 1 SOL
+    const airdropAmount = 1 * web3.LAMPORTS_PER_SOL;
+    const txHash = await pg.connection.requestAirdrop(
+      pg.wallet.publicKey,
+      airdropAmount
+    );
+
+    // Confirm transaction
+    await pg.connection.confirmTransaction(txHash);
+
+    // Fetch new balance
+    const newBalance = await pg.connection.getBalance(pg.wallet.publicKey);
+    console.log(`New balance is ${newBalance} lamports`);
+
+    // Assert balances
+    assert(balance + airdropAmount === newBalance);
+  });
+});
